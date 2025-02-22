cc7318d133c74166a1b3233f392446e49f1b34b7
PC-FISSO\david <davide.bocca@gmail.com>
2025-02-22 14:04:27 +0100

    developments

--- real_script.lua
+++ real_script.lua
@@ -2,36 +2,36 @@ local dt = require "darktable"
 local log = require "lib/dtutils.log"
 
 local DEFAULT_LOG_LEVEL <const> = log.info
+local MISSING_IMAGES_TAG = "missing-images"
 
 log.log_level(DEFAULT_LOG_LEVEL)
 
 local script_data = {}
 
 local function destroy()
-  
+
 end
 
 script_data.destroy = destroy
 
 local function scan()
-  
   local film = dt.collection[1].film
-  
-  log.msg(log.info,"checking folder " .. film.path)
 
-  for i = 1, #film  do
+  log.msg(log.info, "checking folder " .. film.path)
+
+  for i = 1, #film do
     local image = film[i]
 
-    log.msg(log.info,image)
+    log.msg(log.info, image)
 
-    local image_tags= dt.tags.get_tags(image)
+    dt.tags.attach(MISSING_IMAGES_TAG, image)
 
-    for _,v in pairs(image_tags) do
+    local image_tags = dt.tags.get_tags(image)
+
+    for _, v in pairs(image_tags) do
       log.msg(log.info, v)
     end
-
   end
-
 
   -- for _, el in ipairs(dt.films) do
   --   -- log.msg(log.info,el)
